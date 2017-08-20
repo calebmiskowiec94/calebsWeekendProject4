60727996b14ab1cfaa2d3bb0b77f7ece06174292
Caleb <calebmiskowiec94@gmail.com>
2017-08-20 15:37:47 -0500

    button works, yet not sending data to server req.body is empty

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,29 @@ var app = angular.module('TableApp', []);
 app.controller('TableController', ['$http', function ($http) {
     console.log('Table Controller loaded');
     var self = this;
+    self.employees = [];
+    self.newEmployee = {};
 
-    //self.names = [];
+   self.postNewEmployee = function(){
+       console.log("New Employee", self.newEmployee);
+        $http({
+             method: 'POST',
+             url: '/table',
+             data: self.newEmployee             
+         }).then(function (response) {
+             console.log(response);
+             self.getTable();
+         }); // end of $http
+     }; // end of getMessages
 
-    //self.getOwners = function () {
-        //$http({
-           // method: 'GET',
-           // url: '/owners'
-       // }).then(function(response) {
-          //  console.log(response.data);
-          //  self.names = response.data;
-       // })  //then end
-  //  } // getOwners end
-
-
-    //self.getOwners();
-
-    self.employees=[];
+    
+  
     self.getTable = function () {
         $http({
             method: 'GET',
             url: '/table'
         }).then(function(response) {
-            console.log('response.data');
+            // console.log(response.data);
             self.employees = response.data;
         })  //then end
     } // getpets end
--- server/routes/table.js
+++ server/routes/table.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var pool = require('../modules/pool');
 
 router.post('/', function (req, res) {
-    var tableId = req.params.id;
-    console.log('table post');
+    // var tableId = req.params.id;
+    console.log('table post', req.body);
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
             console.log('Error connecting to database', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
-            client.query('INSERT INTO employees (first_name, last_name, job_title, salary) VALUES ($1, $2, $3, $4);', [req.body.first_name, req.body.last_name, req.body.job_title, salary], function (errorMakingQuery, result) {
+            client.query('INSERT INTO employees (first_name, last_name, job_title, salary) VALUES ($1, $2, $3, $4);', [req.body.first_name, req.body.last_name, req.body.job_title, req.body.salary], function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
                     console.log('Error making database query', errorMakingQuery);
@@ -30,6 +30,7 @@ router.get('/', function (req, res) {
             console.log('Error connecting to database', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
+            // console.log('is this working til here?');
             client.query('SELECT * FROM employees', function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
